f537311af7ebf41b7c1dd1f8c30109f1b7c1b6ac
Scott <98685854+PaleProxy@users.noreply.github.com>
2023-03-07 07:31:02 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -11,11 +11,22 @@ engine = create_engine(
 })
 
 
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-    column_names = result.keys()
     jobs = []
     for row in result.all():
-      jobs.append(dict(zip(column_names, row)))
+      jobs.append(dict(row))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"),
+    val=id
+  )
+  rows = result.all()
+  if len(rows) == 0:
+      return None
+  else:
+    return dict(rows[0])
